31e0d12788c86cb69527fcafb0e10b50a05a571a
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-02-25 15:28:28 +0400

    Initial commit

---
+++ organizer.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+/* 
+ * 
+ * 
+ * @param list Array of objects,each object having the following fields:
+ * - SourceFile: The full path to the image file.
+ * - DateTimeOriginal: The original datetime on which the image or video was captured, formatted as yyyy-MM
+ * This list can be obtained by running the command `exiftool -d "%Y-%m" -json -SourceFileName -DateTimeOriginal [Path to folder containing images to organize e.g. "/Users/me/Pictures/**"]`
+ * 
+ * @param saveDirectory the directory where the images will be saved.
+ */
+function organizePhotos(list, saveDirectory){
+  list.forEach(image => {
+
+    const imageOriginalFileName = path.basename(image.SourceFile);
+    const imageNewSavePath = path.join(
+      saveDirectory, 
+      image.DateTimeOriginal? image.DateTimeOriginal : "To organize", 
+      imageOriginalFileName
+    );
+    const imageNewParentDir = path.dirname(imageNewSavePath);
+    if (!fs.existsSync(imageNewParentDir)) {
+      fs.mkdirSync(imageNewParentDir);
+    }
+    fs.renameSync(image.SourceFile, imageNewSavePath);
+  });
+}
